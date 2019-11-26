0d98da0c59dbd49579b03596282831dd05f6f2e2
z7shan <z7shan@edu.uwaterloo.ca>
2019-11-26 08:12:45 -0800

    Final

---
+++ Assets/NotificationController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotificationController : MonoBehaviour
+{
+    float m_timer = 10;
+    
+    private void OnCollisionStay(Collision collision)
+    {
+        m_timer -= Time.deltaTime;
+        if(m_timer == 0)
+        {
+            gameObject.SetActive(false);
+        }
+        GetComponent<TMPro.TextMeshPro>().text = ""+(int)m_timer;
+    }
+}
---
+++ Assets/TreasureManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureManager : MonoBehaviour
+{
+    public TMPro.TextMeshPro TargetText;
+    private Vector3 m_initPosition;
+
+    private void Start()
+    {
+        m_initPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if(transform.position != m_initPosition) TargetText.text = "YOU FOUND IT!";
+    }
+}
